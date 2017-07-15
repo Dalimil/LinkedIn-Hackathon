443bb354eeed5e8e02f4ccc0bcde1a43eaff343b
Dalimil Hajek <dalimilhajek@gmail.com>
2017-07-14 23:43:40 -0700

    Add ProfileCard component

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,36 @@ import logo from './logo.svg';
 import './App.css';
 import ProfileCard from './ProfileCard'
 
+
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.people = [
+      {
+        name: "John Doe",
+        role: "Senior Product Manager",
+        description: "I spent the past 3 years of my career as a senior product manager, working on internal cloud solutions."
+      },
+      {
+        name: "John Doe 2",
+        role: "Senior Product Manager",
+        description: "I spent the past 3 years of my career as a senior product manager, working on internal cloud solutions."
+      }
+    ];
+  }
+  
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h3>LinkedIn Hackathon</h3>
         </div>
-        <ProfileCard />
+        <div className="map">
+          {this.people.map(p => (<ProfileCard {...p} />))}
+        </div>
+        
       </div>
     );
   }
--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
 import './ProfileCard.css';
 
 class ProfileCard extends React.Component {
@@ -20,6 +21,21 @@ class ProfileCard extends React.Component {
       <div className="profilecard-container">
         <div className={"profilecard-inner" + (this.state.expanded ? " expanded" : "")}
           onClick={this.toggleCard.bind(this)} >
+          {this.state.expanded ? (
+            <CSSTransitionGroup
+                transitionName="profilecard-contents-group"
+                transitionAppear={true}
+                transitionAppearTimeout={500}>
+              <div style={{ display: "flex", marginBottom: "1em" }}>
+                <div className="profilecard-photo">OK</div>
+                <div className="profilecard-text-container">
+                  <div className="profilecard-title">{this.props.name}</div>
+                  <div className="profilecard-role">{this.props.role}</div>
+                </div>
+              </div>
+              <div className="profilecard-descr">{this.props.description}</div>
+            </CSSTransitionGroup>
+          ) : ""}
         </div>
       </div>
     );
